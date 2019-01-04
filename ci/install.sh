dc1970f37a423d91f47ba89f8879550a5e685872
David Négrier <d.negrier@thecodingmachine.com>
2019-01-04 16:21:24 +0100

    Autoregister Github access token

--- ci/install.sh
+++ ci/install.sh
@@ -4,4 +4,7 @@ cd /home/docker
 composer create-project thecodingmachine/washingmachine washingmachine ^2.0 --stability=dev
 cd -
 
+# Register token
+composer config --global github-oauth.github.com "$GITHUB_ACCESS_TOKEN"
+
 composer install --prefer-dist
